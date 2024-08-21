6b5fa705f7a395e9316f5d9359c71f92e8250630
dmartimarti <dmartimarti@gmail.com>
2024-08-21 14:39:00 +0100

    Update concat_genes.py

--- code/concat_genes.py
+++ code/concat_genes.py
@@ -3,16 +3,18 @@ import argparse
 from Bio import SeqIO
 from tqdm import tqdm
 
+# usage: python code/concat_genes.py data/genes data/concatenated_genes.fasta
+
 def concatenate_genes_from_folder(folder_path, output_file):
     # Dictionary to store concatenated sequences for each genome
     concatenated_sequences = {}
     
     # List all fasta files in the folder
-    fasta_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.fasta')]
-    
+    fasta_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.aln')]
+    print(f"Found {len(fasta_files)} fasta files in the folder.")
     for fasta_file in tqdm(fasta_files, total=len(fasta_files)):
         with open(fasta_file, 'r') as handle:
-            for record in SeqIO.parse(handle, "fasta"):
+            for record in tqdm(SeqIO.parse(handle, "fasta")):
                 genome_id = record.id
                 sequence = str(record.seq)
                 
@@ -33,7 +35,7 @@ def main():
     parser.add_argument('output_file', type=str, help="Output file name for concatenated sequences.")
     
     args = parser.parse_args()
-    
+    print(args)
     concatenate_genes_from_folder(args.folder_path, args.output_file)
 
 if __name__ == "__main__":
